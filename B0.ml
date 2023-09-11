663cb94385bd0427f4279ce4d5d11525d17053c8
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-09-11 12:07:04 +0200

    Track b0.

--- B0.ml
+++ B0.ml
@@ -15,32 +15,30 @@ let brr_poked = B0_ocaml.libname "brr.poked"
 (* Units *)
 
 let brr_lib =
-  let srcs = Fpath.[ `Dir (v "src") ] in
+  let srcs = [`Dir ~/"src"] in
   let requires = [js_of_ocaml_compiler_runtime] in
   B0_ocaml.lib brr ~doc:"Brr JavaScript FFI and browser API" ~srcs ~requires
 
 let brr_ocaml_poke_lib =
-  let srcs = Fpath.[ `Dir (v "src/ocaml_poke") ] in
+  let srcs = [`Dir ~/"src/ocaml_poke"] in
   let requires = [brr] in
   let doc = "OCaml poke objects interaction" in
   B0_ocaml.lib brr_ocaml_poke ~doc ~srcs ~requires
 
 let brr_ocaml_poke_ui_lib =
-  let srcs = Fpath.[ `Dir (v "src/ocaml_poke_ui") ] in
+  let srcs = [`Dir ~/"src/ocaml_poke_ui"] in
   let requires = [brr; brr_ocaml_poke] in
   let doc = "OCaml poke user interface (toplevel)" in
   B0_ocaml.lib brr_ocaml_poke_ui ~doc ~srcs ~requires
 
 let brr_poke_lib =
-  let srcs = Fpath.[ `Dir (v "src/poke") ] in
-  let requires =
-    [js_of_ocaml_compiler_runtime; js_of_ocaml_toplevel; brr]
-  in
+  let srcs = [`Dir ~/"src/poke"] in
+  let requires = [js_of_ocaml_compiler_runtime; js_of_ocaml_toplevel; brr] in
   let doc = "Poke explicitely" in
   B0_ocaml.lib brr_poke ~doc ~srcs ~requires
 
 let brr_poked_lib =
-  let srcs = Fpath.[ `Dir (v "src/poked") ] in
+  let srcs = [`Dir ~/"src/poked"] in
   let requires = [brr_poke] in
   let doc = "Poke by side effect" in
   B0_ocaml.lib brr_poked ~doc ~srcs ~requires
@@ -48,55 +46,69 @@ let brr_poked_lib =
 (* Web extension *)
 
 let console =
-  let srcs = Fpath.[ `Dir (v "src/console");
-               `X (v "src/console/ocaml_console.js"); (* GNGNGNGN *)
-               `X (v "src/console/devtools.js");
-               `X (v "src/console/highlight.pack.js");
-               (* FIXME we want something like ext_js *) ]
+  let doc = "Browser developer tool OCaml console" in
+  let srcs =
+    [ `Dir ~/"src/console";
+      (* FIXME we want something like ext_js *)
+      `X ~/"src/console/ocaml_console.js"; (* GNGNGNGN *)
+      `X ~/"src/console/devtools.js";
+      `X ~/"src/console/highlight.pack.js" ]
   in
   let requires = [brr; brr_ocaml_poke; brr_ocaml_poke_ui] in
-  let comp_mode = `Whole and source_map = Some `Inline in
-  let comp = Cmd.(arg "--pretty") in
-  let meta = B0_jsoo.meta ~requires ~comp ~comp_mode ~source_map () in
-  let doc = "Browser developer tool OCaml console" in
-  B0_jsoo.web "ocaml_console" ~doc ~srcs ~meta
+  let meta =
+    B0_meta.empty
+    |> ~~ B0_jsoo.compilation_mode `Whole
+    |> ~~ B0_jsoo.source_map (Some `Inline)
+    |> ~~ B0_jsoo.compile_opts (Cmd.arg "--pretty")
+  in
+  B0_jsoo.html_page "ocaml_console" ~requires ~doc ~srcs ~meta
 
-let poke =
-  let srcs = Fpath.[ `File (v "test/poke.ml"); `File (v "test/base.css") ] in
-  let requires = [brr; brr_poked] in
-  let meta = B0_jsoo.meta ~requires ~toplevel:true () in
+let test_poke =
   let doc = "OCaml console test" in
-  B0_jsoo.web "poke" ~doc ~srcs ~meta
+  let srcs =
+    [`File ~/"test/poke.ml";
+     `File ~/"test/base.css"]
+  in
+  let requires = [brr; brr_poked] in
+  let meta = B0_meta.empty |> B0_meta.tag B0_jsoo.toplevel in
+  B0_jsoo.html_page "test_poke" ~requires ~doc ~srcs ~meta
 
 let top =
-  let srcs = Fpath.[ `File (v "test/top.ml");
-(* FIXME js_of_ocaml chokes `File "src/console/highlight.pack.js"; *)
-                     `File (v "src/console/ocaml_console.css") ] in
+  let doc = "In page toplevel test" in
+  let srcs = [
+    `File ~/"test/top.ml";
+    (* FIXME js_of_ocaml chokes `File "src/console/highlight.pack.js";
+       FIXME it's likely fixed by now. *)
+    `File ~/"src/console/ocaml_console.css" ] in
   let requires =
     [ js_of_ocaml_compiler_runtime;
       brr; brr_ocaml_poke_ui; brr_poke; brr_ocaml_poke]
   in
-  let comp_mode = `Whole in
-  let meta = B0_jsoo.meta ~requires ~comp_mode ~toplevel:true () in
-  let doc = "In page toplevel test" in
-  B0_jsoo.web "top" ~doc ~srcs ~meta
+  let meta =
+    B0_meta.empty
+    |> ~~ B0_jsoo.compilation_mode `Whole
+    |> B0_meta.tag B0_jsoo.toplevel
+  in
+  B0_jsoo.html_page "top" ~requires ~doc ~srcs ~meta
 
 (* Tests and samples *)
 
-let test_assets = Fpath.[ `File (v "test/base.css") ]
+let test_assets = [ `File ~/"test/base.css" ]
 
 let test ?(requires = [brr]) n ~doc =
   let srcs = `File (Fpath.v (Fmt.str "test/%s.ml" n)) :: test_assets in
-  let meta = B0_jsoo.meta ~requires () in
-  B0_jsoo.web n ~doc ~srcs ~meta
+  B0_jsoo.html_page n ~requires ~doc ~srcs
 
 let test_module ?doc top m requires  =
   let test = Fmt.str "test_%s" (String.Ascii.uncapitalize m) in
   let doc = Fmt.str "Test %s.%s module" top m in
   let srcs = `File (Fpath.v (Fmt.str "test/%s.ml" test)) :: test_assets in
-  let comp = Cmd.(arg "--pretty") in
-  let meta = B0_jsoo.meta ~requires ~comp () in
-  B0_jsoo.web test ~doc ~srcs ~meta
+  let meta =
+    B0_meta.empty
+    |> ~~ B0_jsoo.compile_opts Cmd.(arg "--pretty")
+    |> ~~ B0_show_url.url (`In (`Unit_dir, Fpath.(v test + ".html")))
+  in
+  B0_jsoo.html_page test ~requires ~doc ~srcs ~meta
 
 let hello = test "test_hello" ~doc:"Brr console hello size"
 let test_base64 = test_module "Brr" "Base64" [brr]
@@ -116,73 +128,63 @@ let test_webgpu = test_module "Brr_webgpu" "Gpu" [brr]
 let test_worker = test_module "Brr" "Worker" [brr]
 
 let min =
-  let srcs = Fpath.[ `File (v "test/min.ml"); `File (v "test/min.html") ] in
+  let srcs = [ `File ~/"test/min.ml"; `File ~/"test/min.html" ] in
   let requires = [brr] in
-  let meta = B0_jsoo.meta ~requires () in
-  B0_jsoo.web "min" ~doc:"Brr minimal web page" ~srcs ~meta
+  B0_jsoo.html_page "min" ~requires ~doc:"Brr minimal web page" ~srcs
 
 let nop =
-  let srcs = Fpath.[ `File (v "test/nop.ml") ] in
-  let meta = B0_jsoo.meta ~requires:[] () in
-  B0_jsoo.web "nop" ~doc:"js_of_ocaml nop web page" ~srcs ~meta
+  let srcs = [ `File ~/"test/nop.ml" ] in
+  B0_jsoo.html_page "nop" ~doc:"js_of_ocaml nop web page" ~srcs
+
+(* Actions *)
+
+let update_console =
+  let doc = "Update dev console" in
+  B0_action.make' ~units:[console] ~doc "update-console" @@
+  fun _ env ~args ->
+  let jsfile = "ocaml_console.js" in
+  let src = B0_env.in_unit_dir env console ~/jsfile in
+  let dst = B0_env.in_scope_dir env Fpath.(~/"src/console" / jsfile) in
+  Os.File.copy ~force:true ~make_path:false ~src dst
 
 (* Packs *)
 
-let test_pack = (* FIXME add stuff in b0 *)
+let test_pack = (* FIXME b0 add stuff for testing *)
   let us = [ test_console ] in
-  let meta = B0_meta.(empty |> tag test) in
-  B0_pack.v ~locked:false "test" ~doc:"Brr test suite" ~meta us
+  let meta = B0_meta.empty |> B0_meta.tag B0_meta.test in
+  B0_pack.make ~locked:false "test" ~doc:"Brr test suite" ~meta us
 
 let jsoo_toplevels =
-  (* FIXME this is wrong and make that nice to write *)
-  let tops = B0_unit.has_meta B0_jsoo.toplevel in
+  (* FIXME this is wrong and make that nice to write
+     Not sure why this is wrong in fact. *)
+  let tops = B0_unit.has_tag B0_jsoo.toplevel in
   let us = List.filter tops (B0_unit.list ()) in
   let doc = "Units with toplevel (slow to build)" in
-  B0_pack.v ~locked:false "tops" ~doc us
+  B0_pack.make ~locked:false "tops" ~doc us
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> add authors ["The brr programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/brr"
-    |> add online_doc "https://erratique.ch/software/brr/doc/"
-    |> add licenses ["ISC"; "BSD-3-Clause"]
-    |> add repo "git+https://erratique.ch/repos/brr.git"
-    |> add issues "https://github.com/dbuenzli/brr/issues"
-    |> add description_tags
+    B0_meta.empty
+    |> ~~ B0_meta.authors ["The brr programmers"]
+    |> ~~ B0_meta.maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> ~~ B0_meta.homepage "https://erratique.ch/software/brr"
+    |> ~~ B0_meta.online_doc "https://erratique.ch/software/brr/doc/"
+    |> ~~ B0_meta.licenses ["ISC"; "BSD-3-Clause"]
+    |> ~~ B0_meta.repo "git+https://erratique.ch/repos/brr.git"
+    |> ~~ B0_meta.issues "https://github.com/dbuenzli/brr/issues"
+    |> ~~ B0_meta.description_tags
       [ "reactive"; "declarative"; "frp"; "front-end"; "browser";
         "org:erratique"]
-    |> add B0_opam.Meta.build
+    |> ~~ B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"]]|}
-    |> tag B0_opam.tag
-    |> add B0_opam.Meta.depends
+    |> ~~ B0_opam.depends
       [ "ocaml", {|>= "4.08.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
         "topkg", {|build & >= "1.0.3"|};
         "js_of_ocaml-compiler", {|>= "4.1.0"|};
-        "js_of_ocaml-toplevel", {|>= "4.1.0"|};
-      ]
+        "js_of_ocaml-toplevel", {|>= "4.1.0"|} ]
+      |> B0_meta.tag B0_opam.tag
   in
-  B0_pack.v "default" ~doc:"brr package" ~meta ~locked:true @@
+  B0_pack.make "default" ~doc:"brr package" ~meta ~locked:true @@
   B0_unit.list ()
-
-(* Cmdlets *)
-
-let update_console =
-  B0_cmdlet.v ~doc:"Update dev console" "update-dev-console" @@ fun env args ->
-  B0_cmdlet.exit_of_result @@
-  (* FIXME so much path boilerplate. Summon units/packs to be build
-     and get info directly. *)
-  let b0_dir = B0_cmdlet.Env.b0_dir env in
-  let variant = "user" in
-  let build_dir = B0_dir.build_dir ~b0_dir ~variant in
-  let unit_dir = B0_dir.unit_build_dir ~build_dir ~name:"ocaml_console" in
-  let src = Fpath.(unit_dir / "ocaml_console.js") in
-  let dst = Fpath.(v "src" / "console" / "ocaml_console.js") in
-  let dst = B0_cmdlet.in_scope_dir env dst in
-  let cmd = Cmd.(arg "b0" % "-u" % "ocaml_console" % "-u" % "poke") in
-  Result.bind (Os.Cmd.run cmd) @@ fun () ->
-  Os.File.copy ~force:true ~make_path:false ~src dst
---
+++ DEVEL.md
@@ -0,0 +1,10 @@
+# Testing 
+
+There are a few test see `b0 list`, the can be built and shown in your
+browser with:
+
+    b0 -- .show-url test_c2d
+    b0 -- .show-url test_gl
+    b0 -- .show-url test_audio
+    … 
+
